e7f48d9c0530da584ba093474c5a1c043794fa06
Fadi Aboona <fadi.aboona23@gmail.com>
2023-03-17 18:19:05 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(i){
+  let message = 'This one is on me!'
+  if (i < 400)
+  return message
+  else if (399 < i, i < 2000)
+  return "That will be twenty bucks."
+  else if (i > 2000, i < 2500)
+  return "I will gladly take your thirty bucks."
+  else if (i > 2500)
+  return "No can do."
+ 
+ 
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return city === `NYC`? `Ok, sounds good.`: `No go.`
+
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
+function switchOnCharmFromTip(tip){
+  switch(tip) {
+    case 'generous':
+      return "Thank you so much."
+    case "not as generous":
+      return 'Thank you.'
+    default:
+      return 'Bye.'
+
+  }
 }
